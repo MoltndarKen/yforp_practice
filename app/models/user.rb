773657d033b868b26c888d4451bb6f3d75f20bc7
kohei_uetan <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-09-06 03:17:42 +0900

    # what 画像を登録できるところまでやる

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   belongs_to :group
+  mount_uploader :avatar, AvatarUploader
   attr_accessor :group_key
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150905173431) do
+ActiveRecord::Schema.define(version: 20150905180706) do
 
   create_table "groups", force: true do |t|
     t.string   "key"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20150905173431) do
     t.string   "first_name"
     t.string   "family_name_kana"
     t.string   "first_name_kana"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
